8dd10e194efe75d496ecf2475af8eb169e242bff
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-10 17:06:34 -0500

    Añadir botones + y - para modificar el score de cada jugador. Section 3 clase 23

--- client/main.js
+++ client/main.js
@@ -7,7 +7,14 @@ import { Players } from './../imports/api/players';
 
 const renderPlayers = (playersList) => {
 	return playersList.map( (player) => {
-		return <p key={player._id}><strong><i>{player.name}</i></strong> has {player.score} point(s).</p>
+		return (
+			<p key={player._id}>
+				<strong><i>{player.name}</i></strong> has {player.score} point(s).
+				<button onClick={() => Players.update({_id: player._id},  {$inc:{ score: 1 }} )}>+1</button>
+				<button onClick={() => Players.update(player._id,  {$inc:{ score: -1 }} )}>-1</button>
+				<button onClick={() => Players.remove({ _id: player._id})}>X</button>
+			</p>
+		)
 	});
 };
 
