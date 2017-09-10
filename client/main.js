70db83ef1cc31ec901f4bb8d9ecbe73d62e1cd34
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-10 15:12:51 -0500

    Section 3, clase 18

--- client/main.js
+++ client/main.js
@@ -5,47 +5,31 @@ import { Tracker } from 'meteor/tracker';
 
 import { Players } from './../imports/api/players';
 
-Tracker.autorun( function(){
-    console.log('Players List: ', Players.find().fetch());
-});
-
-const players = [
-    {
-        _id: 'id_1',
-        name: 'Lauren Smith',
-        score: 4
-    },{
-        _id: 'id_2',
-        name: 'Juan Camilo Quintero',
-        score: 7
-    },{
-        _id: 'id_3',
-        name: 'Laura Hernandez',
-        score: 2
-    },{
-        _id: 'id_4',
-        name: 'Clara Fernandez',
-        score: 5
-    }
-];
-
 const renderPlayers = function(playersList) {
-    return playersList.map( function(player) {
-        return <p key={player._id}><strong><i>{player.name}</i></strong> has {player.score} point(s).</p>
-    });
+	return playersList.map( function(player) {
+		return <p key={player._id}><strong><i>{player.name}</i></strong> has {player.score} point(s).</p>
+	});
 };
 
 Meteor.startup(function() {
-    let name = 'Juan Camilo Quintero';
-    let title = 'Score Keep App';
-    let jsx = (
-        <div>
-            <h1>{title}</h1>
-            <p>Hola Component, me llamo <strong>{name}</strong></p>
-            {renderPlayers(players)}
-        </div>
-    );
-    ReactDOM.render( jsx, document.getElementById('app'));
+	Tracker.autorun( function(){
+		let players = Players.find().fetch();
+		let name = 'Juan Camilo Quintero';
+		let title = 'Score Keep App';
+		let jsx = (
+			<div>
+				<h1>{title}</h1>
+				<p>Hola Component, me llamo <strong>{name}</strong></p>
+				{renderPlayers(players)}
+			</div>
+		);
+		ReactDOM.render( jsx, document.getElementById('app'));
+	});
+
+	Players.insert({
+		name: 'JuanMArta HErnandez Ramirez',
+		score: 90
+  });
 });
 
 
--- server/main.js
+++ server/main.js
@@ -2,12 +2,6 @@ import {Meteor} from 'meteor/meteor';
 import { Players } from './../imports/api/players';
 
 Meteor.startup(function() {
-  Players.insert(
-    {
-      name: 'JuanCammmiloQ',
-      score: 95
-    }
-  );
-  console.log(Players.find().fetch());
+  
    
 });
