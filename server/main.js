af8033c95d4e6ff054fe63ecb43f3eb6b9c132d0
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-10 16:35:43 -0500

    Refactoring arrow functions - Section 3 clase 21

--- server/main.js
+++ server/main.js
@@ -1,8 +1,6 @@
 import {Meteor} from 'meteor/meteor';
 import { Players } from './../imports/api/players';
 
-Meteor.startup(function() {
-  let numbers = [9 , 99 , 19 , 34, 45];
-  let newNumbers = numbers.map( (number) => number + 1);
-    console.log(newNumbers);
+Meteor.startup( () => {
+ 
 });
