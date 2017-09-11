e1341664ce2984a55aa6e8ba6f3dea494d9a2b4c
Juan Camilo Quintero <juancamiloq1@gmail.com>
2017-09-11 11:53:17 -0500

    Bind This, Section 4 clase 31

--- imports/ui/AddPlayer.js
+++ imports/ui/AddPlayer.js
@@ -18,7 +18,7 @@ export default class AddPlayer extends Component{
 	}
 	render(){
 		return(
-			<form onSubmit={this.handleSubmit}>
+			<form onSubmit={this.handleSubmit.bind(this)}>
 				<input type="text" name="playerName" placeholder="Nombre del jugador"/>
 				<button>Añadir Jugador</button>
 			</form>
